e694123931526d35356c9172bc939b82b1196618
William Puk <williampuk@gmail.com>
2025-05-18 03:27:47 +0800

    support grpc-web message. show more possible scalar values

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"math"
 	"os"
 	"strings"
 	"unicode/utf8"
@@ -67,7 +69,7 @@ func parseMessage(data []byte) ([]Field, int, error) {
 			if cn < 0 {
 				return nil, totalConsumed, fmt.Errorf("invalid varint at offset %d", len(data)-len(remaining))
 			}
-			value = v
+			value = interpretVarint(v)
 			consumed = cn
 
 		case protowire.Fixed32Type:
@@ -75,7 +77,7 @@ func parseMessage(data []byte) ([]Field, int, error) {
 			if cn < 0 {
 				return nil, totalConsumed, fmt.Errorf("invalid fixed32 at offset %d", len(data)-len(remaining))
 			}
-			value = v
+			value = interpretFixed32(v)
 			consumed = cn
 
 		case protowire.Fixed64Type:
@@ -83,7 +85,7 @@ func parseMessage(data []byte) ([]Field, int, error) {
 			if cn < 0 {
 				return nil, totalConsumed, fmt.Errorf("invalid fixed64 at offset %d", len(data)-len(remaining))
 			}
-			value = v
+			value = interpretFixed64(v)
 			consumed = cn
 
 		case protowire.BytesType:
@@ -121,6 +123,58 @@ func parseMessage(data []byte) ([]Field, int, error) {
 	return fields, totalConsumed, nil
 }
 
+// zigZagDecode32 decodes a ZigZag-encoded 32-bit value
+func zigZagDecode32(n uint32) int32 {
+	return int32(n>>1) ^ -int32(n&1)
+}
+
+// zigZagDecode64 decodes a ZigZag-encoded 64-bit value
+func zigZagDecode64(n uint64) int64 {
+	return int64(n>>1) ^ -int64(n&1)
+}
+
+func interpretVarint(v uint64) interface{} {
+	// Try different interpretations of the varint
+	var interpretations []string
+
+	// Unsigned integers
+	interpretations = append(interpretations, fmt.Sprintf("[uint64]: %v", v))
+	interpretations = append(interpretations, fmt.Sprintf("[uint32]: %v", uint32(v)))
+
+	// Signed integers
+	interpretations = append(interpretations, fmt.Sprintf("[int64]: %v", int64(v)))
+	interpretations = append(interpretations, fmt.Sprintf("[int32]: %v", int32(v)))
+	interpretations = append(interpretations, fmt.Sprintf("[sint64]: %v", zigZagDecode64(v)))
+	interpretations = append(interpretations, fmt.Sprintf("[sint32]: %v", zigZagDecode32(uint32(v))))
+
+	// Boolean
+	if v == 0 {
+		interpretations = append(interpretations, fmt.Sprintf("[bool]: %v", false))
+	} else if v == 1 {
+		interpretations = append(interpretations, fmt.Sprintf("[bool]: %v", true))
+	}
+
+	// Enum
+	interpretations = append(interpretations, fmt.Sprintf("[enum]: %v", v))
+
+	return interpretations
+}
+func interpretFixed32(v uint32) interface{} {
+	var interpretations []string
+	interpretations = append(interpretations, fmt.Sprintf("[fixed32]: %v", v))
+	interpretations = append(interpretations, fmt.Sprintf("[float]: %v", math.Float32frombits(v)))
+	interpretations = append(interpretations, fmt.Sprintf("[sfixed32]: %v", int32(v)))
+	return interpretations
+}
+
+func interpretFixed64(v uint64) interface{} {
+	var interpretations []string
+	interpretations = append(interpretations, fmt.Sprintf("[fixed64]: %v", v))
+	interpretations = append(interpretations, fmt.Sprintf("[double]: %v", math.Float64frombits(v)))
+	interpretations = append(interpretations, fmt.Sprintf("[sfixed64]: %v", int64(v)))
+	return interpretations
+}
+
 // formatFields recursively formats decoded fields into a human-readable string.
 func formatFields(fields []Field, indent string) string {
 	var builder strings.Builder
@@ -128,6 +182,11 @@ func formatFields(fields []Field, indent string) string {
 		builder.WriteString(fmt.Sprintf("%sTag %d (%s): ", indent, field.Tag, typeIntToNames[field.Type]))
 
 		switch v := field.Value.(type) {
+		case []string:
+			// Multiple interpretations (varint, fixed32/64)
+			builder.WriteString("{\n")
+			builder.WriteString(fmt.Sprintf("%s  %s\n", indent, strings.Join(v, ", ")))
+			builder.WriteString(indent + "}\n")
 		case uint32:
 			builder.WriteString(fmt.Sprintf("uint32: %d (0x%x)\n", v, v))
 		case uint64:
@@ -147,6 +206,29 @@ func formatFields(fields []Field, indent string) string {
 	return builder.String()
 }
 
+func extractProtobufBytesIfRecognizedAsGrpcWebMsg(data []byte) []byte {
+	if len(data) < 5 {
+		// Data too short for gRPC-Web frame
+		return data
+	}
+
+	compressionFlag := data[0]
+	if compressionFlag != 0 {
+		// Compressed messages are not supported
+		return data
+	}
+
+	messageLength := binary.BigEndian.Uint32(data[1:5])
+	messageStart := 5
+	messageEnd := messageStart + int(messageLength)
+	if messageEnd > len(data) {
+		// Parsed message length exceeds available data; therefore the input can't be gRPC-Web message
+		return data
+	}
+	fmt.Println("The input will be parsed as gRPC-Web message")
+	return data[messageStart:messageEnd]
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <encoded_string>")
@@ -161,6 +243,7 @@ func main() {
 	}
 
 	// Parse the Protobuf message
+	msgBytes = extractProtobufBytesIfRecognizedAsGrpcWebMsg(msgBytes)
 	fields, _, err := parseMessage(msgBytes)
 	if err != nil {
 		panic(fmt.Sprintf("Parse error: %v", err))
